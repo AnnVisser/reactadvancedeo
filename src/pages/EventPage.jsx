032b4679e72117ba1f5c6565771145637ee76461
Annemarie Visser <annvisser@ziggo.nl>
2023-09-27 13:44:47 +0200

    basestructure

---
+++ src/pages/EventPage.jsx
@@ -0,0 +1,23 @@
+export const EventPage = () => {
+	return (
+		<>
+			<h1>Event Page</h1>
+		</>
+	);
+};
+
+// Event page
+
+// The event page shows the following details:  title, description, image, startTime, endTime, categories and by who it’s created (display their name and image).
+
+// You can click on an edit button where the user can edit the details shown on the page. This has to be done in a modal or on the same page, not an external page.
+
+// The back-end server data is updated after.
+
+// A succes or fail message is shown after a successful update.
+
+// You can click on a delete button to delete the event.
+
+// A delete query is sent to delete the data in the back-end.
+
+// After deleting an event, you get redirected to the Events page.
---
+++ src/pages/EventsPage.jsx
@@ -0,0 +1,21 @@
+export const EventsPage = () => {
+	return (
+		<>
+			<h1>Events Page:</h1>
+		</>
+	);
+};
+
+// Events page
+
+// The page shows a list of all events that are retrieved from the back-end server with the following details: title, description, image, startTime, endTime and categories.
+
+// The user can click on an event that leads them to the ‘Event’ page using React Router.
+
+// There is a button that allows the user to add new events using a form.
+
+// A query to add the event to the server is sent as well.
+
+// You can search through the events based on the name of the event.
+
+// You can filter through the list/search results based on categories.
